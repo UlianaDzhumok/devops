65251beb494dae9d9d60608ae68ffca5c9b3db44
UlianaDzhumok <uliana.dzhumok@gmail.com>
2020-05-06 14:23:20 +0000

    FEAT: Adding Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="udzhumok/devops:demo"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run prediction\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=8000 --labels app=prediction
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward prediction 8000:80
 
